7a23eb1bcb7229c36cdfb9125411ff6b16bd2692
lin5606 <linh562k6@gmail.com>
2026-04-02 22:36:13 +0700

    sửa enum của table

--- btloop/HighlandsCoffee/src/HighlandsCoffee/model/Table.java
+++ btloop/HighlandsCoffee/src/HighlandsCoffee/model/Table.java
@@ -1,19 +1,22 @@
 package model;
 
+import enums.TableStatus;
+
 public class Table {
     private String Table_id;
     private String Table_number;
     private int Seat;
-    private String Status;
+    private TableStatus Table_status;
     public Table(){
-        Table_id=Table_number=Status="";
+        Table_id=Table_number="";
         Seat=0;
+        Table_status=TableStatus.TRONG;
     }
-    public Table(String id, String number, int seat, String status){
+    public Table(String id, String number, int seat, TableStatus tablestatus){
         this.Table_id=id;
         this.Table_number=number;
         this.Seat=seat;
-        this.Status=status;
+        this.Table_status=tablestatus;
     }
     public String getTable_id(){
         return Table_id;
@@ -24,8 +27,8 @@ public class Table {
     public int getSeat(){
         return Seat;
     }
-    public String getStatus(){
-        return Status;
+    public TableStatus getStatus(){
+        return Table_status;
     }
     public void setTable_id(String id){
         this.Table_id=id;
@@ -36,7 +39,7 @@ public class Table {
     public void setSeat(int seat){
         this.Seat=seat;
     }
-    public void setStatus(String status){
-        this.Status=status;
+    public void setStatus(TableStatus tablestatus){
+        this.Table_status=tablestatus;
     }
 }
