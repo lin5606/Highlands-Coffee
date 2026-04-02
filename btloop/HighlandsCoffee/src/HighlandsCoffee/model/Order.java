5740a5300edf08b79a58019b8472b8635250fb2d
lin5606 <linh562k6@gmail.com>
2026-04-02 22:12:20 +0700

    order (constructor, get, set)

--- btloop/HighlandsCoffee/src/HighlandsCoffee/model/Order.java
+++ btloop/HighlandsCoffee/src/HighlandsCoffee/model/Order.java
@@ -1,5 +1,53 @@
 package model;
 
+import java.util.Date;
+import enums.OrderStatus;
+
 public class Order {
-    
+    private String Order_id;
+    private Date Order_date;
+    private float Total_amount;
+    private OrderStatus Order_status;
+
+    //constructor
+    public Order(){
+        this.Order_id="";
+        this.Order_date=null;
+        this.Total_amount=0;
+        this.Order_status=OrderStatus.PENDING;
+    }
+    public Order(String orderid, Date orderdate, float totalamount, OrderStatus orderstatus){
+        this.Order_id=orderid;
+        this.Order_date=orderdate;
+        this.Total_amount=totalamount;
+        this.Order_status=orderstatus;
+    }
+
+    //getter
+    public String getOrder_id(){
+        return Order_id;
+    }
+    public Date getOrder_date(){
+        return Order_date;
+    }
+    public float getTotal_amount(){
+        return Total_amount;
+    }
+    public OrderStatus getOrder_status(){
+        return Order_status;
+    }
+
+    //setter
+    public void setOrder_id(String orderid){
+        this.Order_id=orderid;
+    }
+    public void setOrder_date(Date orderdate){
+        this.Order_date=orderdate;
+    }
+    public void setTotal_amount(float totalamount){
+        this.Total_amount=totalamount;
+    }
+    public void setOrder_status(OrderStatus orderstatus){
+        this.Order_status=orderstatus;
+    }
 }
--- btloop/HighlandsCoffee/src/HighlandsCoffee/model/OrderItem.java
+++ btloop/HighlandsCoffee/src/HighlandsCoffee/model/OrderItem.java
@@ -1,5 +1,63 @@
 package model;
 
 public class OrderItem {
-    
+
+    private String Order_item_id;
+    private Order order;
+    private Product product;
+    private int Quantity;
+    private float Price;
+
+    //constructor
+    public OrderItem(){
+        this.Order_item_id="";
+        this.order= null;
+        this.product=null;
+        this.Quantity=0;
+        this.Price=0;
+    }
+    public OrderItem(String orderitemid, Order order, Product product, int quantity) {
+        this.Order_item_id=orderitemid;
+        this.order = order;
+        this.product = product;
+        this.Quantity = quantity;
+        this.Price = product.getPrice(); 
+    }
+
+    //getter
+    public String getOrder_item_id(){
+        return Order_item_id;
+    }
+    public Order getorder(){
+        return order;
+    }
+    public Product getProduct(){
+        return product;
+    }
+    public int getQuantity(){
+        return Quantity;
+    }
+    public float getPrice(){
+        return Price;
+    }
+    public double getSubtotal() {
+        return Quantity * Price;
+    }
+
+    //setter
+    public void setOrder_item_id(String orderitemid){
+        this.Order_item_id=orderitemid;
+    }
+    public void setorder(Order order){
+        this.order=order;
+    }
+    public void setProduct(Product product){
+        this.Product=product;
+        if(product!=null){
+            this.Price=product.getPrice();
+        }
+    }
+    public void setQuantiry(int quantity){
+        this.Quantity=quantity;
+    }
 }
